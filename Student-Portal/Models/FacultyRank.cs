240911d0bd28a1e8ba01fd426580b98fb9721faf
Bolaji Olawale <bolaji.olawale007@gmail.com>
2020-07-02 23:45:39 +0100

    added faculty rank attribute to model

--- Student-Portal/Models/FacultyRank.cs
+++ Student-Portal/Models/FacultyRank.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace Student_Portal.Models
 {
+    [Table("FacultyRank")]
     public class FacultyRank
     {
+        [Key]
+        [Column("RankId")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Display(Name ="Faculty Rank")]
+        public string Rank { get; set; }
     }
 }
