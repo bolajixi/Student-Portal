e588f65b7866e873e3dac5b60a86801b92d367d8
Bolaji Olawale <bolaji.olawale007@gmail.com>
2020-07-03 08:43:13 +0100

    create model for faculty and their rank

---
+++ Student-Portal/Models/FacultyAndRank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Student_Portal.Models
+{
+    public class FacultyAndRank
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int RankId { get; set; }
+
+        public string Username { get; set; }
+    }
+}
