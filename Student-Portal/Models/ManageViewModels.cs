70d59177967bc678f372ffa71be967a6baa84b51
Bolaji Olawale <bolaji.olawale007@gmail.com>
2020-08-02 12:45:08 +0100

    added profile viewmodel

--- Student-Portal/Models/ManageViewModels.cs
+++ Student-Portal/Models/ManageViewModels.cs
@@ -83,4 +83,78 @@ namespace Student_Portal.Models
         public string SelectedProvider { get; set; }
         public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
     }
+
+    public class ProfileViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Password")]
+        //public string Password { get; set; }
+
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
+        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        //public string ConfirmPassword { get; set; }
+
+        //[Display(Name = "Role")]
+        //public string RoleeName { get; set; }
+
+        //[Display(Name = "Faculty Rank")]
+        //public string Rank { get; set; }
+
+        //-------------------------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------------------------------
+
+        //Personal Details
+        [Required(ErrorMessage = "Oops! You missed your first name")]
+        [Display(Name = "First Name")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Oops! You missed your last name")]
+        [Display(Name = "Last Name")]
+        public string Lastname { get; set; }
+
+        //[Required(ErrorMessage = "I think you forgot pop's name")]
+        [Display(Name = "Father's/Guardian's Name")]
+        public string Fathername { get; set; }
+
+        [Display(Name = "Mother's Name")]
+        public string Mothername { get; set; }
+
+        [Required(ErrorMessage = "Sneaky but you have to set a date")]
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid Date")]
+        public string DateOfBirth { get; set; }
+
+        [Required]
+        //[Display(Name = "Gender")]
+        public Gender gender { get; set; }
+
+        //---------------------------------------------------------------------------------------------------------
+        //Communication Details
+
+
+        //[Required(ErrorMessage = "Mobile number is required")]
+        [Display(Name = "Mobile")]
+        public string Mobile { get; set; }
+
+        [Required]
+        [Display(Name = "Address 1")]
+        public string Address1 { get; set; }
+
+        [Display(Name = "Address 2")]
+        public string Address2 { get; set; }
+
+        [Display(Name = "City/Town")]
+        public string City { get; set; }
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+    }
 }
