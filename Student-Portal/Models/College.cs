cb5168eb265b506c67a776e1fda7964ba737541f
Bolaji Olawale <bolaji.olawale007@gmail.com>
2020-08-11 22:11:55 +0100

    added college model

---
+++ Student-Portal/Models/College.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Student_Portal.Models
+{
+    public class College
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "College Name")]
+        public string CollegeName { get; set; }
+
+        [Display(Name ="Number of Departments")]
+        public int numDept { get; set; }
+    }
+}
