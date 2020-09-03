1b163697caa7593276ae3e6c9af8e52dafde3195
Bolaji Olawale <bolaji.olawale007@gmail.com>
2020-09-03 20:11:04 +0100

    Add view model for approved and unapproved courses

---
+++ Student-Portal/Models/CoursesViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Student_Portal.Models
+{
+    public class ApproveViewModel
+    {
+        public IEnumerable<UnappovedCoursesModel> UnappovedCourses { get; set; }
+        public IEnumerable<AppovedCoursesModel> AppovedCourses { get; set; }
+    }
+
+    public class UnappovedCoursesModel
+    {
+        //public Course Course { get; set; }
+        //public RegisterCourse RegisterCourse { get; set; }
+
+        [Display(Name = "Course Name")]
+        public string CourseName { get; set; }
+
+        [Display(Name = "Course Code")]
+        public String CourseCode { get; set; }
+
+        [Display(Name = "Course Unit")]
+        public int CourseUnit { get; set; }
+
+        public int CourseId { get; set; }
+
+
+    }
+    public class AppovedCoursesModel
+    {
+        //public Course Course { get; set; }
+        //public RegisterCourse RegisterCourse { get; set; }
+
+        [Display(Name = "Course Name")]
+        public string CourseName { get; set; }
+
+        [Display(Name = "Course Code")]
+        public String CourseCode { get; set; }
+
+        [Display(Name = "Course Unit")]
+        public int CourseUnit { get; set; }
+
+        public int CourseId { get; set; }
+
+
+    }
+}
